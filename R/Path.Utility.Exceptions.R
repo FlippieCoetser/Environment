5ca3292e68e2eae3a77c0a61df7e2fd1d4929161
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-19 10:38:41 -0700

    EXCEPTIONS: input |> exception[['InvalidPath']](path) ThrowsExceptionIfInputIsTrue -> PASS

--- R/Path.Utility.Exceptions.R
+++ R/Path.Utility.Exceptions.R
@@ -1,5 +1,9 @@
 Path.Utility.Exceptions <- \() {
   exceptions <- list()
-  exceptions[['InvalidPath']] <- \() { }
+  exceptions[['InvalidPath']] <- \(invoke, path = NULL) {
+    if (invoke) {
+      stop("Invalid path: ", path, ".")
+    } 
+  }
   return(exceptions)
 }
