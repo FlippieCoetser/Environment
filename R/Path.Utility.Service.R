ce76f9bbafbcb9f76e717297ddf96724210aac9f
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-19 08:18:25 -0700

    SERVICE: Path.Utility.Service() ReturnsListContainingNormalizePath -> PASS

--- R/Path.Utility.Service.R
+++ R/Path.Utility.Service.R
@@ -2,5 +2,6 @@ Path.Utility.Service <- \() {
   services <- list()
   services[['GetUserHomePath']] <- \() {}
   services[['GetConfigFilename']] <- \() {}
+  services[['NormalizePath']] <- \(path) {}
   return(services)
 }
