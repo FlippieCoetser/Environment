c6da03ca59ea39a9fd533a6289967a12700c53b2
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-20 02:39:32 -0700

    CODE RUB: Remove Call Trace

--- R/Path.Utility.Exceptions.R
+++ R/Path.Utility.Exceptions.R
@@ -2,7 +2,7 @@ Path.Utility.Exceptions <- \() {
   exceptions <- list()
   exceptions[['InvalidPath']] <- \(invoke, path = NULL) {
     if (invoke) {
-      stop("Invalid path: ", path, ".")
+      stop("Invalid path: ", path, ".", call. = FALSE)
     } 
   }
   return(exceptions)
