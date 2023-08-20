7a915745af1563ce26080c449b58ba192a4f49ac
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-20 02:07:58 -0700

    CODE RUB: Update Regular Expression

--- R/Path.Utility.Validation.R
+++ R/Path.Utility.Validation.R
@@ -1,7 +1,7 @@
 Path.Utility.Validation <- \() {
   validators <- list()
   validators[['Path']] <- \(path) {
-    pattern <- "(^([a-zA-Z]:)(\\[a-zA-Z0-9_-]*)*)|((/[a-zA-Z0-9_-]*)*)"
+    pattern <- "(^([a-zA-Z]:)(\\[a-zA-Z0-9_-.]*)*)|((/[a-zA-Z0-9_-.]*)*)"
     check <- pattern |> grepl(path)
 
     if (check == TRUE) {
