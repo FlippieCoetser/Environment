9909caf313919ba2d071d955233d406213147826
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-22 11:14:43 -0700

    EXCEPTIONS: input |> exception[['PathNotFound']](path) ThrowsExceptionIfInputIsTrue -> PASS

--- R/Session.Utility.Exceptions.R
+++ R/Session.Utility.Exceptions.R
@@ -1,7 +1,11 @@
 Session.Utility.Exceptions <- \() {
   exceptions <- list()
   exceptions[['NavigateToFileExceptions']] <- \() {}
-  exceptions[['PathNotFound']] <- \() { }
+  exceptions[['PathNotFound']] <- \(invoke, path = NULL) {
+    if (invoke) {
+      stop("Path not found: ", path, ".", call. = FALSE)
+    } 
+  }
   exceptions[['FileNotFound']] <- \() { }
   return(exceptions)
 }
