714973f61aaca538dbb5fb6db569ca014fc63dcf
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-22 11:07:16 -0700

    EXCEPTIONS: Session.Utility.Exceptions() ReturnsListContainingFileNotFoundException -> PASS

--- R/Session.Utility.Exceptions.R
+++ R/Session.Utility.Exceptions.R
@@ -2,5 +2,6 @@ Session.Utility.Exceptions <- \() {
   exceptions <- list()
   exceptions[['NavigateToFileExceptions']] <- \() {}
   exceptions[['PathNotFound']] <- \() { }
+  exceptions[['FileNotFound']] <- \() { }
   return(exceptions)
 }
