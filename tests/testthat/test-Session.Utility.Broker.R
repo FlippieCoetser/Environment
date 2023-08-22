d0f8d24bc04bf6c99971f1ece7c76bff3b3569d2
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-22 02:02:19 -0700

    BROKER: Session.Utility.Broker() ReturnsListContainingGetEnvVariable -> FAIL

--- tests/testthat/test-Session.Utility.Broker.R
+++ tests/testthat/test-Session.Utility.Broker.R
@@ -48,4 +48,11 @@ describe("When operations <- Session.Utility.Broker()", {
     # Then
     operations[["VSCodeInUse"]] |> expect.exist()
   })
+  it("then operations should contains GetEnvVariable operation.", {
+    # Given
+    operations <- Session.Utility.Broker()
+
+    # Then
+    operations[["GetEnvVariable"]] |> expect.exist()
+  })
 })
