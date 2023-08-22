26cdb2a7a883fae1d59754989da750410cb529d2
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-22 10:53:11 -0700

    EXCEPTIONS: Session.Utility.Exceptions() ReturnsListOfExceptions -> FAIL

--- tests/testthat/test-Session.Utility.Exceptions.R
+++ tests/testthat/test-Session.Utility.Exceptions.R
@@ -4,3 +4,13 @@ describe('Session.Utility.Exceptions', {
     Session.Utility.Exceptions |> expect.exist()
   })
 })
+
+describe("When exceptions <- Session.Utility.Exceptions()",{
+  it("then exceptions should be a list.", {
+    # Given
+    exceptions <- Session.Utility.Exceptions()
+
+    # Then
+    exceptions |> expect.list()
+  })
+})
