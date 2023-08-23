11ae21830a6931d562bd2e05bcee8a3dcfd36a13
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-23 06:59:12 -0700

    VALIDATION: throw.warning() |> validate[['NavigationRepsonse']]() ThrowsFileNotFoundExceptionIfResponseThrowsWarningContainingFileNotFound -> FAIL

--- tests/testthat/test-Session.Utility.Validation.R
+++ tests/testthat/test-Session.Utility.Validation.R
@@ -26,13 +26,25 @@ describe("When response |> validate[['NavigationResponse']]()",{
   it("then a PathNotFound exception should be thrown is response throws a warning containing path not found",{
     # Given
     validate <- Session.Utility.Validation()
-    response <- \() {
+    throw.warning <- \() {
       warning('path[1]="C:/Users/InvalidPath/Documents/.Renviron": The system cannot find the path specified.')
     }
 
     expected.error <- "Path not found: C:/Users/InvalidPath/Documents/.Renviron."
     
     # Then
-    response() |> validate[["NavigationResponse"]]() |> expect.error(expected.error)
+    throw.warning() |> validate[["NavigationResponse"]]() |> expect.error(expected.error)
+  })
+  it("then a FileNotFound exception should be thrown if response throws a warning containing file not found",{
+    # Given
+    validate <- Session.Utility.Validation()
+    throw.warning <- \() {
+      warning('path[1]="C:/Users/Analyst/Documents/check.txt": The system cannot find the file specified.')
+    }
+
+    expected.error <- "File not found: C:/Users/Analyst/Documents/check.txt."
+    
+    # Then
+    throw.warning() |> validate[["NavigationResponse"]]() |> expect.error(expected.error)
   })
 })
