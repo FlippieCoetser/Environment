6483a288435ca681f6d986215f57494dbc377aa6
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-23 06:43:59 -0700

    VALIDATION: Session.Utility.Validation() ReturnsListOfValidators -> FAIL

--- tests/testthat/test-Session.Utility.Validation.R
+++ tests/testthat/test-Session.Utility.Validation.R
@@ -4,3 +4,13 @@ describe('Session.Utility.Validation', {
     Session.Utility.Validation |> expect.exist()
   })
 })
+
+describe("When validators <- Session.Utility.Validation()",{
+  it("then validators should be a list.", {
+    # Given
+    validators <- Session.Utility.Validation()
+
+    # Then
+    validators |> expect.list()
+  })
+})
