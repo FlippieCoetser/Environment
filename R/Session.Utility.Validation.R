d57112976cb747e955e3b2d738583f09c9ea5e59
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-23 06:59:51 -0700

    VALIDATION: throw.warning() |> validate[['NavigationRepsonse']]() ThrowsExceptionIfResponseThrowsWarning -> PASS

--- R/Session.Utility.Validation.R
+++ R/Session.Utility.Validation.R
@@ -1,5 +1,9 @@
 Session.Utility.Validation <- \() {
+  exception <- Session.Utility.Exceptions()
+  
   validations <- list()
-  validations[['NavigationResponse']] <- \() {}
+  validations[['NavigationResponse']] <- \(response) {
+    response |> tryCatch(warning = exception[['NavigateToFileExceptions']])
+  }
   return(validations)
 }
