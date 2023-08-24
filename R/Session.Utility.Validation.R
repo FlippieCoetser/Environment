4c91a3e41106f065230ddab47d31889970e1def0
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-24 01:59:44 -0700

    CODE RUB: Align Indentation

--- R/Session.Utility.Validation.R
+++ R/Session.Utility.Validation.R
@@ -5,22 +5,22 @@ Session.Utility.Validation <- \() {
   validations[['NavigationResponse']] <- \(response) {
     response |> tryCatch(warning = exception[['NavigateToFileExceptions']])
   }
-  validations[['Filepath']]  <- \(filepath) {
+  validations[['Filepath']]           <- \(filepath) {
     pattern <- "^(([a-zA-Z]:)(/[a-zA-Z0-9_.-]+)+/[a-zA-Z0-9_.-]*[a-zA-Z0-9])|(/([a-zA-Z0-9_.-]*/?)*[a-zA-Z0-9_.-]*[a-zA-Z0-9])$"
     pattern |> grepl(filepath) |> isFALSE() |> exception[['InvalidFilepath']](filepath)
     return(filepath)
   }
-  validations[['IsEmpty']] <- \(value) {
+  validations[['IsEmpty']]            <- \(value) {
     value |> (\(x){ x == "" })()
   } 
-  validations[['IsNull']] <- \(value) {
+  validations[['IsNull']]             <- \(value) {
     value |> is.null()
   }
-  validations[['Name']] <- \(name) {
+  validations[['Name']]               <- \(name) {
     name |> validations[['IsNull']]() |> exception[['NameIsNull']]()
     return(name)
   }
-  validations[['Value']] <- \(value, name) {
+  validations[['Value']]              <- \(value, name) {
     value |> validations[['IsEmpty']]() |> exception[['ValueIsEmpty']](name)
     return(value)
   }
