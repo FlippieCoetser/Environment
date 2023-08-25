80ee50a586db8ccf99dcc08040ab57873a785764
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-25 10:33:14 -0700

    CODE RUB: Align Spacing

--- R/Session.Utility.Service.R
+++ R/Session.Utility.Service.R
@@ -1,6 +1,7 @@
 Session.Utility.Service <- \(broker) {
   validate <- Session.Utility.Validation()
   exception <- Session.Utility.Exceptions()
+  
   services <- list()
   services[['HasRStudioAPI']]     <- \() {
     broker[["HasRStudioAPI"]]()
