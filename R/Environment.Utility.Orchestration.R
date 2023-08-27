996b7418a215a5a2dcf7f44f5ae18368b8588a3f
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-27 02:25:22 -0700

    DOCUMENTATION: Add Roxygen Documentation

--- R/Environment.Utility.Orchestration.R
+++ R/Environment.Utility.Orchestration.R
@@ -1,3 +1,20 @@
+#' Manage Environment Variables
+#'
+#' @description
+#' `.Renviron` configuration file is well suited to management sensitive information. 
+#' This `Environment` package makes defining and reading environment variables straightforward.
+#' 
+#' `Environment.Utility.Orchestration()` returns a set of utility functions that can be used to interact with `.Renviron` files:
+#' * The first function: `OpenConfigFile`, will open an existing or a new empty `.Renviron` configuration file.
+#' Users can then add environment variables by defining `'key'='value'` pairs.
+#' * The second function: `GetEnvVariable`, will read the value of the matching key (name) from the configuration file.
+#' * The third function: `CacheEnvVariable`, will cache the value of the matching key (name) in the current R session.
+#' @usage NULL
+#' @returns A `list` of utility functions: 
+#' * `OpenConfigFile()`
+#' * `GetEnvVariable(name)`
+#' * `CacheEnvVariable(name, value)`
+#' @export
 Environment.Utility.Orchestration <- \(...) {
   path <- 
     Path.Utility.Broker()  |> 
