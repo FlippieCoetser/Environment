3e61958373b1ee31709ac610787442444cafea10
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-27 02:04:30 -0700

    ORCHESTRATION: orchestrate[['OpenConfigFile']]() OpenRenvironFileInIDE -> PASS

--- R/Environment.Utility.Orchestration.R
+++ R/Environment.Utility.Orchestration.R
@@ -1,5 +1,27 @@
-Environment.Utility.Orchestration <- \() {
+Environment.Utility.Orchestration <- \(...) {
+  path <- 
+    Path.Utility.Broker()  |> 
+    Path.Utility.Service() |> 
+    Path.Utility.Processing()
+
+  session <- 
+    Session.Utility.Broker()  |> 
+    Session.Utility.Service() |> 
+    Session.Utility.Processing()
+
+  arguments <- list(...)
+  if(arguments[['path']] |> is.null() |> isFALSE()) {
+    path <- arguments[['path']]
+  }
+  if(arguments[['session']] |> is.null() |> isFALSE()) {
+    session <- arguments[['session']]
+  }
+
   orchestrations <- list()
-  orchestrations[['OpenConfigFile']] <- \() {}
+  orchestrations[['OpenConfigFile']] <- \() {
+    path[['GetConfigFilepath']]()   |> 
+    path[['EnsureFilepathExist']]() |>
+    session[['OpenFilepath']]()
+  }
   return(orchestrations)
 }
